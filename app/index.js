6ee5a7c23e1cdd5a1fd7d818e4d604a5323fdec5
Stephanie Winn <stephanie.n.winn@gmail.com>
2017-03-31 15:05:36 -0500

    begin javascript sort with shuffle

--- app/index.js
+++ app/index.js
@@ -1,1 +1,22 @@
 import 'whatwg-fetch';
+
+const Shuffle = window.shuffle;
+
+const el = function (element) {
+  this.element = element;
+
+
+  this.addShuffleEventListerners();
+
+  this.Shuffle = new Shuffle(element, {
+    itemSelector: '.picture-item',
+    sizer: element.querySelector('.my-sizer-element'),
+  });
+
+  this._activeFilters = [];
+
+  this.addFilterButtons();
+  this.addSorting();
+
+  this.mode = 'exclusive';
+};
